b979df918b995b14b133c671958f3f0d7ff623c2
ArthurDev-A <arthurdearaujocustodio0Wgmail.com>
2024-12-20 05:03:07 -0300

    Finalização da atividade de template

--- Atividade-Templates/routes/index.js
+++ Atividade-Templates/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Meu site' });
 });
 
 module.exports = router;
