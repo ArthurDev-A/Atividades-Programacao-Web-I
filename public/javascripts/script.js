adbd55e80f6e2d62ac240bf8c31db5a881eadc5f
Arthur de Araujo Custódio <141514740+ArthurDev-A@users.noreply.github.com>
2024-12-20 04:08:59 -0300

    Delete public directory

--- public/javascripts/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('submit').addEventListener('click', function() {
-// O evento de clique não causará o recarregamento da página
-const nome = document.getElementById('nome').value;
-console.log("Nome capturado:", nome);
-
-window.location.href = '/users/' + nome;
-});
